bae69ad18a810c11eab328967afc7182cd642611
Robert Burek <robertburek@wp.pl>
2019-03-02 22:51:09 +0100

    Zmodyfikowałem noweTlo.js na bardziej uniwersalne rozwiązanie
    	wystarczy zmienić ilePrac oraz dodać klasę do plikiu style.css
    	o nazwie nowaX, x-kolejna moja praca, dokładniej to screen

--- js/nowetlo.js
+++ js/nowetlo.js
@@ -1,21 +1,18 @@
 ﻿var i=1;
+var ilePrac=4;
 
 function wPrawoCSS(element) {
-  if (i==4) i=0;
+  if (i==ilePrac) i=0;
   i++;
-	var el = document.getElementById(element);
-  if (i==1) el.className = 'oferta nowa1';
-  if (i==2) el.className = 'oferta nowa2';
-  if (i==3) el.className = 'oferta nowa3';
-  if (i==4) el.className = 'oferta nowa4';
-}
+  var el = document.getElementById(element);
+  el.className = 'oferta nowa'+i;
+  }
 
 function wLewoCSS(element) {
-  if (i==1) i=5;
+  if (i==1) i=ilePrac+1;
   i--;
   var el = document.getElementById(element);
-  if (i==1) el.className = 'oferta nowa1';
-  if (i==2) el.className = 'oferta nowa2';
-  if (i==3) el.className = 'oferta nowa3';
-  if (i==4) el.className = 'oferta nowa4';
-}
+  el.className = 'oferta nowa'+i;
+  //  Dodaj class .nowaX do arkuszy styli style.css
+  //  oraz zmień ilePrac na nową wartość.
+  }
