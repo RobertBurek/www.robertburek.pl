f523b09653bb59a19992162932d076f4e5c0b71e
Robert Burek <robertburek@wp.pl>
2019-07-13 00:32:44 +0200

    Totalna zmiana sposobu wyświetlania dyplomów
       Lista dyplomów jest w pliku dyplomy.js i tam jest generowany html
       wystarczy w nim dodać jeden wiersz i skrypt wygeneruje wszystko

--- js/dyplomy.js
+++ js/dyplomy.js
@@ -1,41 +1,83 @@
-var images = document.querySelectorAll('img');
+
 var index = 1;
 
-function showImage(n){
+const dyplomy = [];
+dyplomy[21] = ['image/Dyplomy/WprowadzenieDoGitIGitHub_udamy.jpg', 'udemy']
+dyplomy[20] = ['image/Dyplomy/WireframingWithBalsamiq_udemy.jpg', 'udemy']
+dyplomy[19] = ['image/Dyplomy/NajpopularniejszeBibliotekiJAVA_strefaKursow.jpg', 'strefaKursow']
+dyplomy[18] = ['image/Dyplomy/SpringFrameworkOdPodstaw_strefaKursow.jpg', 'strefaKursow']
+dyplomy[17] = ['image/Dyplomy/BeginnerJavaJavaFXMavenJenkins_udemy.jpg', 'udemy']
+dyplomy[16] = ['image/Dyplomy/JavaOdPodstawDoEksperta_udemy.jpg', 'udemy']
+dyplomy[15] = ['image/Dyplomy/JavaWzorceProjektowe_strefaKursow.jpg', 'strefaKursow']
+dyplomy[14] = ['image/Dyplomy/JavaŚredniozaawansowana_strefaKursow.jpg', 'strefaKursow']
+dyplomy[13] = ['image/Dyplomy/DobrePraktykiPracyWZespole_strefaKursow.jpg', 'strefaKursow']
+dyplomy[12] = ['image/Dyplomy/JDBCandMySQL_udemy.jpg', 'udemy']
+dyplomy[11] = ['image/Dyplomy/JDBCinSimpleWay _udemy.jpg', 'udemy']
+dyplomy[10] = ['image/Dyplomy/FundamentyJezykaJava_strefaKursow.jpg', 'strefaKursow']
+dyplomy[9] = ['image/Dyplomy/Certyfikat_codeORG.jpg', 'strefaKursow']
+dyplomy[8] = ['image/Dyplomy/EComerceWPraktyce_uouw.jpg', 'uouw']
+dyplomy[7] = ['image/Dyplomy/CertyfikatGIT_eduwebpl.jpg', 'pionowo']
+dyplomy[6] = ['image/Dyplomy/KursProgramowaniaWJava_strefaKursow.jpg', 'strefaKursow']
+dyplomy[5] = ['image/Dyplomy/AngOdZeraDoBohatIA2_uouw.jpg', 'uouw']
+dyplomy[4] = ['image/Dyplomy/AngielskiPoLatachA1_uouw.jpg', 'uouw']
+dyplomy[3] = ['image/Dyplomy/AngOdZeraDoBohatIIIA1A2_uouw.jpg', 'uouw']
+dyplomy[2] = ['image/Dyplomy/JakCięWidząAutoprezentacja_uouw.jpg', 'uouw']
+dyplomy[1] = ['image/Dyplomy/AngOdZeraDoBohatIIA1_uouw.jpg', 'uouw']
+dyplomy[0] = ['image/Dyplomy/ScenaDlaKażdego_uouw.jpg', 'uouw']
+
+
+const div = document.querySelector(".wstawiany");
+
+for (let i = dyplomy.length - 1; i >= 0; i--) {
+    var img = document.createElement('img');
+    img.src = dyplomy[i][0];
+    img.classList = dyplomy[i][1];
+    img.alt = (dyplomy[i][0]).substr(14, (dyplomy[i][0]).length - 4);
+    div.appendChild(img);
+    // console.log('obrazek:', dyplomy[i][0], ' class:', dyplomy[i][1], ' i:', i)
+}
+
+
+var images = document.querySelectorAll('img');
+// console.log('ilość dyplomów', dyplomy.length)
+
+function showImage(n) {
     images[n].classList.add('visible');
 }
 
-function hideImage(n){
+function hideImage(n) {
     images[n].classList.remove('visible');
 }
 
 images[0].classList.add('visible');
 showImage(1);
 
-function update(){
-if (index===images.length-1)
-    index = 1;
-if(index===1)
-    index = images.length-1;
+function update() {
+    if (index === images.length - 1)
+        index = 1;
+    if (index === 1)
+        index = images.length - 1;
 }
 
-function goNext(){
+function goNext() {
     hideImage(index);
-    if (index===images.length-1)
+    if (index === images.length - 1)
         index = 0;
     index++;
     showImage(index);
+    // console.log(index)
 }
 
-function goPrev(){
+function goPrev() {
     hideImage(index);
-    if (index===1)
-        index = 20;
+    if (index === 1)
+        index = images.length;
     index--;
     showImage(index);
+    // console.log(index)
 }
 
-var next =document.querySelector('#next');
-var prev =document.querySelector('#prev');
+var next = document.querySelector('#next');
+var prev = document.querySelector('#prev');
 next.addEventListener('click', goNext);
 prev.addEventListener('click', goPrev);
