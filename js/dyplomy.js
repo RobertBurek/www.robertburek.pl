a758356b3526d39a80f987e71999b11164d7fc9a
Robert Burek <robertburek@wp.pl>
2019-10-17 23:45:59 +0200

    Szybki kurs HTML5 i CSS3

--- js/dyplomy.js
+++ js/dyplomy.js
@@ -2,8 +2,9 @@
 var index = 1;
 
 const dyplomy = [];
-dyplomy[33] = ['image/Dyplomy/JavaDeveloper_strefaKursow.png', 'strefaKursow']
-dyplomy[32] = ['image/Dyplomy/AdministratorBazDanych_strefaKursow.png', 'strefaKursow']
+dyplomy[34] = ['image/Dyplomy/JavaDeveloper_strefaKursow.png', 'strefaKursow']
+dyplomy[33] = ['image/Dyplomy/AdministratorBazDanych_strefaKursow.png', 'strefaKursow']
+dyplomy[32] = ['image/Dyplomy/SzybkiKursHTML5CSS3_udemy.jpg', 'udemy']
 dyplomy[31] = ['image/Dyplomy/Podstawy programowania - back end_altkom.jpg', 'altkom']
 dyplomy[30] = ['image/Dyplomy/KursMicrosoftSQLServerOdPodstaw_strefaKursow.jpg', 'strefaKursow']
 dyplomy[29] = ['image/Dyplomy/KursMongoDBnowoczesneBazyDanych_strefaKursow.jpg', 'strefaKursow']
